e4d29d15dd025b4a1b68da363b374d288daa4c0f
Leandro <leanfiadone@gmail.com>
2024-07-14 01:31:43 -0300

    router dom finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,34 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
 
+  const { authUser,  isLoading} = useAuthContext();
+  console.log("Auth user", authUser)
+
+  if (isLoading) return null
 
   return (
     <>
       <div className="p-4 h-screen flex items-cent justify-center">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <Login /> : <Navigate to={"/"} />}
+          />
         </Routes>
       </div>
     </>
